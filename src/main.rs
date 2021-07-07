e06035be52ee3e118b21f9ada0281afcbc74a3b3
Matthias Henning <matze_henning@gmx.de>
2021-07-07 15:36:41 +0200

    Weiter

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use num::complex::Complex;
+use std::time::{Duration, Instant};
 
 
 fn main() {
 
-    for real in 0 .. 10  {
+    for real in 0 .. 3  {
         
-        for img in 0 .. 10 {
+        for img in 0 .. 3 {
             let a = Complex::new(real as f32, img as f32); 
             let b = a.sin();
         
@@ -13,5 +14,19 @@ fn main() {
         }        
     }
 
-   
+    benchmark();
 }
+
+fn benchmark() {
+        let mut count = 0;
+        let time_limit = Duration::new(1,0);
+        let start = Instant::now();
+    
+        while (Instant::now() - start) < time_limit {
+            count += 1;
+        }
+        println!("{}", count);
+    }
+
+   
+

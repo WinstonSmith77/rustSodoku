f56135f5194a55394b3f328c0a1b332b6d401dd9
Matthias Henning <matze_henning@gmx.de>
2021-07-07 15:21:56 +0200

    Weiter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use num::complex::Complex;
+
+
 fn main() {
-    println!("Hello, world!");
+
+    for real in 0 .. 10  {
+        
+        for img in 0 .. 10 {
+            let a = Complex::new(real as f32, img as f32); 
+            let b = a.sin();
+        
+            println!("Hello, world! {} {}", a, b);
+        }        
+    }
+
+   
 }
